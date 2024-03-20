f4ac3a7293ec6f1b0262e952567939e1e427c1e5
Henrik Soerensen <henrik@voi.network>
2024-03-20 12:04:14 -0400

    Add retry logic on failing requests

--- tools/catch-catchpoint.go
+++ tools/catch-catchpoint.go
@@ -15,44 +15,54 @@ func main() {
 		Timeout: time.Second * 30,
 	}
 
-	resp, err := client.Get("https://testnet-api.voi.nodly.io/v2/status")
-	if err != nil {
+	var resp *http.Response
+	var err error
+
+	for i := 0; i < 10; i++ {
+		resp, err = client.Get("https://testnet-api.voi.nodly.io/v2/status")
+		if err == nil && resp.StatusCode == http.StatusOK {
+			break
+		}
+
+		fmt.Printf("Attempt %d failed with error: %v. Retrying in 10 seconds...\n", i+1, err)
+		time.Sleep(10 * time.Second)
+	}
+
+	if err != nil || resp.StatusCode != http.StatusOK {
 		fmt.Println("Error:", err)
+		fmt.Println("HTTP request failed with status:", resp.StatusCode)
 		return
 	}
+
 	defer resp.Body.Close()
 
-	if resp.StatusCode == http.StatusOK {
-		bodyBytes, err := io.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println("Error reading body:", err)
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading body:", err)
+		return
+	}
+
+	var result map[string]interface{}
+	json.Unmarshal(bodyBytes, &result)
+
+	if lastCatchpoint, ok := result["last-catchpoint"]; ok {
+		catchpoint, ok := lastCatchpoint.(string)
+		if !ok {
+			fmt.Println("Error: last-catchpoint is not a string")
 			return
 		}
 
-		var result map[string]interface{}
-		json.Unmarshal(bodyBytes, &result)
+		fmt.Println("Catching up to catchpoint:", catchpoint)
+		cmd := exec.Command("/node/bin/goal", "-d", "/algod/data", "node", "catchup", catchpoint)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
 
-		if lastCatchpoint, ok := result["last-catchpoint"]; ok {
-			catchpoint, ok := lastCatchpoint.(string)
-			if !ok {
-				fmt.Println("Error: last-catchpoint is not a string")
-				return
-			}
-
-			fmt.Println("Catching up to catchpoint:", catchpoint)
-			cmd := exec.Command("/node/bin/goal", "-d", "/algod/data", "node", "catchup", catchpoint)
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-
-			err := cmd.Run()
-			if err != nil {
-				fmt.Println("Error running command:", err)
-				return
-			}
-		} else {
-			fmt.Println("last-catchpoint not found in the response")
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println("Error running command:", err)
+			return
 		}
 	} else {
-		fmt.Println("HTTP request failed with status:", resp.StatusCode)
+		fmt.Println("last-catchpoint not found in the response")
 	}
 }
