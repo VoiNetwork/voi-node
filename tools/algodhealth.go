060daac9a01abc9eac49f2d3b850bb6e47acf3cb
Henrik Soerensen <henrik@voi.network>
2024-03-20 12:31:54 -0400

    Add error messaging

--- tools/algodhealth.go
+++ tools/algodhealth.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"time"
@@ -13,11 +14,15 @@ func main() {
 
 	resp, err := client.Get("http://127.0.0.1:8080/health")
 	if err != nil {
+		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error: HTTP request failed with status: %d\n", resp.StatusCode)
 		os.Exit(1)
+	} else {
+		fmt.Printf("Algod healthcheck passed.")
 	}
 }
